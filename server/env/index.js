4e8e29939e4a906709709698234d1fc3d26b84e0
Taka Okunishi <okunishinishi@gmail.com>
2017-08-04 00:28:18 +0900

    refactoring

--- server/env/index.js
+++ server/env/index.js
@@ -6,18 +6,8 @@ const theSeat = require('the-seat')
 const portBase = 5000
 const seat = theSeat()
 const ports = seat.scope('ports')
-const portFor = (name) => {
-  const {has, get, take, canTake} = ports.bind(`${name}@${__dirname}`)
-  if (has()) {
-    return get()
-  }
-  let taking = portBase
-  while (!canTake(taking)) {
-    taking += 1
-  }
-  take(taking)
-  return get()
-}
+const portFor = (name) =>
+  ports.acquire(`${name}@${__dirname}`, port => portBase + 1)
 
 const config = {
   port: require('./port'),
