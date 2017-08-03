b2d520a85c2c3172f1eef150130635d3ab3434fc
Taka Okunishi <okunishinishi@gmail.com>
2017-08-03 22:56:43 +0900

    Use the-seat for ports

--- server/env/index.js
+++ server/env/index.js
@@ -1,15 +1,33 @@
 'use strict'
 
 const theEnv = require('the-env')
-const Container = require('../../misc/docker/Container')
+const theSeat = require('the-seat')
 
-let config = {
+const portBase = 5000
+const seat = theSeat()
+const ports = seat.scope('ports')
+const portFor = (name) => {
+  const {has, get, take, canTake} = ports.bind(`${name}@${__dirname}`)
+  if (has()) {
+    return get()
+  }
+  let taking = portBase
+  while (!canTake(taking)) {
+    taking += 1
+  }
+  take(taking)
+  return get()
+}
+
+const config = {
   port: require('./port'),
   database: require('./database'),
   redis: require('./redis')
 }
 
-const vars = { Container }
+const vars = {
+  portFor
+}
 
-const env = theEnv(config, { vars }).forEnv()
+const env = theEnv(config, {vars, portFor}).forEnv()
 module.exports = env
